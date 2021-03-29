51b0e158f3f206e899e41fffd9f176621b0a9b43
kyori19 <kyori@accelf.net>
2021-03-29 22:49:58 +0900

    Fix foreign constraint name

--- db/migrations/20210321021706_CreateRecordings.up.sql
+++ db/migrations/20210321021706_CreateRecordings.up.sql
@@ -11,13 +11,13 @@ create table epgstation_recordings
 (
     epgstation_id bigint not null unique primary key,
     recording_id  bigint not null unique,
-    foreign key epgstation_recordings_recordings_id_foreign (recording_id) references recordings (id)
+    constraint epgstation_recordings_recording_id_foreign foreign key (recording_id) references recordings (id)
 );
 
 create table discord_recordings
 (
     recording_id bigint       not null unique primary key,
-    foreign key discord_recordings_recordings_id_foreign (recording_id) references recordings (id),
+    constraint discord_recordings_recording_id_foreign foreign key (recording_id) references recordings (id),
     channel_id   varchar(255) not null,
     message_id   varchar(255) not null
 );
